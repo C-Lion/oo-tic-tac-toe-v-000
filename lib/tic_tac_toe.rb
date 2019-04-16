80391db2d4534d16dac76b8f5fc9d6c0c12ae782
C-Lion Stepansky <esstepansky@gmail.com>
2019-04-16 08:30:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,4 +3,15 @@ class TicTacToe
     @board = Array.new(9, " ") 
   end
   
+  WIN_COMBINATIONS = [
+    [0, 1, 2], 
+    [2, 4, 5], 
+    [6, 7, 8], 
+    [0, 2, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]
+    ]
+  
 end
